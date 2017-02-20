2ff27512cfd1a4bd1421b505e94971a1fe8e9ac5
Quintonamore <teas98@gmail.com>
2017-02-20 07:28:37 -0800

    Added a web view so that a user can search to find a recipe they want saved within the app.

--- MyApplication/app/src/main/java/quintonamore/recipies/MainURLenter.java
+++ MyApplication/app/src/main/java/quintonamore/recipies/MainURLenter.java
@@ -4,7 +4,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -25,13 +29,37 @@ public class MainURLenter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_urlenter);
 
+        Toolbar tbar = (Toolbar) findViewById(R.id.urlToolbar);
+        setSupportActionBar(tbar);
+
+        //Web View Setup
+        WebView b = (WebView) findViewById(R.id.webView);
+
+
+        //Setting edits needed to view webpages.
+        b.getSettings().setJavaScriptEnabled(true);
+
+        b.getSettings().setAllowContentAccess(true);
+        b.getSettings().setAllowFileAccess(true);
+        b.getSettings().setDatabaseEnabled(true);
+        b.getSettings().setDomStorageEnabled(true);
+
+        b.setWebViewClient(new WebViewClient());
+
+        //Set the webview to the only site I have set up data scraping from.
+        b.loadUrl("http://www.food.com/recipe");
+
+
     }
 
 
     public void onClick(View view) throws ExecutionException, InterruptedException {
 
-        EditText t = (EditText)findViewById(R.id.url_input);
-        String s = t.getText().toString();
+        //Old get the recipe from text.
+        //EditText t = (EditText)findViewById(R.id.url_input);
+
+        WebView wbv = (WebView) findViewById(R.id.webView);
+        String s = wbv.getUrl();
 
         RetrieveData d = new RetrieveData();
         d.execute(s);
